1a874681ad0df4df5a24b17766857b50bcba6d0f
Sebastian Alvarado <se.alvarado@profesor.duoc.cl>
2021-04-19 20:57:53 -0400

    ejemplo

---
+++ ejemplos.js
@@ -0,0 +1,30 @@
+var verdadero = false
+while(verdadero){
+
+}
+////
+for(var i; i<10; i++) {
+
+}
+////
+if(false) {
+
+}
+else {
+    //otra cosa
+}
+
+
+
+
+///
+do {
+
+} while(false);
+////
+var opt = 1;
+switch(opt) {
+    case 1: break;
+    case 2: break;
+    default: break;
+}
